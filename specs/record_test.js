173dc0f64d86cad6c2ad5a2418b4c14d1e5a3e14
Alice Loudon <alice_loudon@icloud.com>
2017-04-07 10:31:04 +0100

    Record has artist. Pass test

---
+++ specs/record_test.js
@@ -0,0 +1,16 @@
+var assert = require ('assert')
+var Record = require ('../record.js')
+
+describe ('record', function(){
+
+  var record
+
+  beforeEach(function(){
+    record = new Record('The Beatles', 'Abbey Road', 'Rock/Pop', 1500)
+  })
+
+  it('has artist', function(){
+    assert.strictEqual(record.artist ,'The Beatles')
+  })
+
+})
