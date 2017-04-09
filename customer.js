5839c24f3585be73c9274c69b248231944784447
Alice Loudon <alice_loudon@icloud.com>
2017-04-09 17:23:16 +0100

    Customer can compare value of collection with another customer and return a string. Pass test

--- customer.js
+++ customer.js
@@ -1,7 +1,34 @@
 var RecordCollection = require ('./recordCollection.js')
 
-var Customer = function(recordCollection){
+var Customer = function(name, recordCollection){
+  this.name = name
   this.recordCollection = new RecordCollection()
+}
+
+Customer.prototype = {
+
+  compareValue: function(anotherCustomer){
+    var differenceInValue = this.recordCollection.valueOfCollection - anotherCustomer.recordCollection.valueOfCollection
+    return differenceInValue
+  },
+
+  compareValueAndReturnString: function(anotherCustomer){
+    var myValue = this.recordCollection.valueOfCollection
+    var otherCustomersValue = anotherCustomer.recordCollection.valueOfCollection
+
+    if (myValue > otherCustomersValue) {
+      var differenceInValue = this.recordCollection.valueOfCollection - anotherCustomer.recordCollection.valueOfCollection
+      return "Your record collection is " + differenceInValue + "p more valuable than " + anotherCustomer + "'s record collection."
+    }
+    else if (otherCustomersValue > myValue) {
+      var differenceInValue = anotherCustomer.recordCollection.valueOfCollection - this.recordCollection.valueOfCollection
+      return anotherCustomer.name + "'s record collection is " + differenceInValue + "p more valuable than your record collection."
+    }
+    else {
+      return "Your record collection is worth exactly the same as " + anotherCustomer + "'s record collection."
+    }
+  }
+
 }
 
 module.exports = Customer
--- specs/customerTest.js
+++ specs/customerTest.js
@@ -6,11 +6,14 @@ var RecordCollection = require ('../recordCollection.js')
 describe('customer', function(){
 
   var customer
+  var customer2
   var recordCollection
 
   beforeEach(function(){
-    customer = new Customer(recordCollection)
+    customer = new Customer("Alice", recordCollection)
+    customer2 = new Customer("Donald", recordCollection)
     record = new Record('The Clash', 'London Calling', 'Rock', 1000)
+    record2 = new Record('The Who', 'My Generation', 'Rock', 1500)
   })
 
   it('has a record collection of zero', function(){
@@ -20,6 +23,18 @@ describe('customer', function(){
   it('can buy a record', function(){
     customer.recordCollection.addRecord(record)
     assert.strictEqual(customer.recordCollection.inventory.length, 1)
+  })
+
+  it('can compare value of collection with another customer', function(){
+    customer.recordCollection.addRecord(record)
+    customer2.recordCollection.addRecord(record2)
+    assert.strictEqual(customer.compareValue(customer2), -500)
+  })
+
+  it('can compare value of collection and return a string', function(){
+    customer.recordCollection.addRecord(record)
+    customer2.recordCollection.addRecord(record2)
+    assert.strictEqual(customer.compareValueAndReturnString(customer2), "Donald's record collection is 500p more valuable than your record collection.")
   })
 
 })
